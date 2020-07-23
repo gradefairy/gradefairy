d80637949620888f2bd78a79cd099669148342c5
dahee8kim <dahee8kim@gmail.com>
2020-07-24 04:02:39 +0900

    Add: NoticeViewScreen.js

---
+++ screens/NoticeViewScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, Text} from "react-native";
+import GlobalStyles from "../styles/GlobalStyles";
+
+export default function NoticeViewScreen() {
+  return (
+    <View style={GlobalStyles.container}>
+      <Text>NoticeViewScreen.js</Text>
+    </View>
+  );
+}
