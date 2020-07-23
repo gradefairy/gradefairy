232787fd6eddad79fff5a105c1c37bd601455414
dahee8kim <dahee8kim@gmail.com>
2020-07-24 03:16:21 +0900

    Add: NoticeSearchScreen.js

---
+++ screens/NoticeSearchScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View} from "react-native";
+import GlobalStyles from "../styles/GlobalStyles";
+
+export default function NoticeSearchScreen() {
+  return (
+    <View style={GlobalStyles.container}>
+      <Text>NoticeSearchScreen.js</Text>
+    </View>
+  );
+}
