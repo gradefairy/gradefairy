da35654408504becb9b74960b671433b40a93c4f
dahee8kim <dahee8kim@gmail.com>
2020-07-24 03:32:58 +0900

    Add: NoticeFilterScreen.js

---
+++ screens/NoticeFilterScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View} from "react-native";
+import GlobalStyles from "../styles/GlobalStyles";
+
+export default function NoticeFilterScreen() {
+  return (
+    <View style={GlobalStyles.container}>
+      <Text>NoticeFilterScreen.js</Text>
+    </View>
+  );
+}
