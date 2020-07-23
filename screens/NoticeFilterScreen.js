4c56e3707dcd58117aed2a6ce3219fcb9efa926e
dahee8kim <dahee8kim@gmail.com>
2020-07-24 04:46:46 +0900

    Update: Add NavigationHeader

--- screens/NoticeFilterScreen.js
+++ screens/NoticeFilterScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {View, Text} from "react-native";
 import GlobalStyles from "../styles/GlobalStyles";
+import NavigationHeader from "../components/NavigationHeader";
 
-export default function NoticeFilterScreen() {
+export default function NoticeFilterScreen({navigation}) {
   return (
     <View style={GlobalStyles.container}>
+      <NavigationHeader title={"맞춤 공지 설정"} navigation={navigation} />
       <Text>NoticeFilterScreen.js</Text>
     </View>
   );
