099479523361cab5a98c70a88961c9f20d4748fc
Kim Dahee <49140707+dahee8kim@users.noreply.github.com>
2020-07-24 14:35:22 +0900

    Update: Add layout guide comment (#5)
    * Update: Add layout guide comment

--- screens/NoticeFilterScreen.js
+++ screens/NoticeFilterScreen.js
@@ -7,7 +7,9 @@ export default function NoticeFilterScreen({navigation}) {
   return (
     <View style={GlobalStyles.container}>
       <NavigationHeader title={"맞춤 공지 설정"} navigation={navigation} />
+      {/* 여기서부터 맞춤 공지 레이아웃 */}
       <Text>NoticeFilterScreen.js</Text>
+      {/* 여기까지 맞춤 공지 레이아웃 */}
     </View>
   );
 }
