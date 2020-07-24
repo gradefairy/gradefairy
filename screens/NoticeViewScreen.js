14aa936bb40af6e1e84ee59a108bfb3b9f3c874f
dahee8kim <dahee8kim@gmail.com>
2020-07-24 12:34:03 +0900

    Update: Add NavigationHeader

--- screens/NoticeViewScreen.js
+++ screens/NoticeViewScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {View, Text} from "react-native";
 import GlobalStyles from "../styles/GlobalStyles";
+import NavigationHeader from "../components/NavigationHeader";
 
-export default function NoticeViewScreen() {
-  return (
-    <View style={GlobalStyles.container}>
-      <Text>NoticeViewScreen.js</Text>
-    </View>
-  );
+export default class NoticeViewScreen extends React.Component {
+  render() {
+    const noticeData = this.props.route.params;
+    return (
+      <View style={GlobalStyles.container}>
+        <NavigationHeader title={noticeData.title} navigation={this.props.navigation} />
+        <Text>NoticeViewScreen.js</Text>
+      </View>
+    );
+  }
 }
