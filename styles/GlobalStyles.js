bfd9674af6687daed0693f477c8595b4b2094eb6
dahee8kim <dahee8kim@gmail.com>
2020-07-24 12:37:34 +0900

    Update: Add common table styles

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -9,5 +9,30 @@ export default StyleSheet.create({
   safeArea: {
     flex: 1,
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  tableContainer: {
+    flex: 1
+  },
+  tableHeaderContainer: {
+    borderBottomWidth: 2,
+    borderBottomColor: "#ddd",
+    flexDirection: "row",
+    height: 50,
+    alignItems: "center"
+  },
+  tableHeader: {
+    fontWeight: "bold",
+    fontSize: 16,
+    textAlign: "center"
+  },
+  tableDataContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee",
+    flexDirection: "row",
+    height: 60,
+    alignItems: "center"
+  },
+  tableData: {
+    textAlign: "center"
   }
 });
