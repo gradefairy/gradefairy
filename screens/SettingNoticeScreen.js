d9adec1db4ca3c3a96e83063991382caf7eff749
HYUNAHSIM <42240771+HYUNAHSHIM@users.noreply.github.com>
2020-08-01 21:30:11 +0900

    feat: settings 추가 (#6)
    Co-authored-by: Kim Dahee

--- screens/SettingNoticeScreen.js
+++ screens/SettingNoticeScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {StyleSheet, Text, View, TouchableOpacity, FlatList} from "react-native";
 import GlobalStyles from "../styles/GlobalStyles";
-import {StyleSheet, View} from "react-native";
 import NavigationHeader from "../components/NavigationHeader";
+
+const noticeList = [
+  {
+    title: "notice1",
+    day: "2020-07-20"
+  },
+  {
+    title: "notice2",
+    day: "2020-07-21"
+  }
+]
 
 export default class SettingNoticeScreen extends React.Component {
   constructor(props) {
@@ -14,8 +25,38 @@ export default class SettingNoticeScreen extends React.Component {
           title={"앱 공지사항"}
           navigation={this.props.navigation} />
         {/* 여기서부터 앱 공지사항 레이아웃 */}
+        <FlatList
+          data={noticeList}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              style={styles.notice}
+              /*onPress={() => {
+              }}*/>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.day}>{item.day}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item, index) => index}/>
         {/* 여기까지 앱 공지사항 레이아웃 */}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  notice: {
+    height: 70,
+    alignItems: "flex-start",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#eee"
+  },
+  title: {
+    fontSize: 17,
+    paddingBottom: 5
+  },
+  day: {
+    color: "#ee"
+  }
+});
