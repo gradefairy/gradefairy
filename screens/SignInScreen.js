89139e7ab664206f46afcf95ae17126b914e7f97
Kim Dahee <49140707+dahee8kim@users.noreply.github.com>
2020-09-15 08:27:07 +0900

    Merge: Add Sign In, Sign Up routine (#18)
    * Modified: Add Login routine
    * Modified: Add Sign Up routine
    * Modified: Edit route link

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {View, TextInput, TouchableOpacity,Text} from "react-native";
+import {View, TextInput, TouchableOpacity,Text, Alert} from "react-native";
 import GlobalStyles from "../styles/GlobalStyles";
 import SignInStyles from "../styles/SignInStyles";
 import {SafeAreaView} from "react-navigation";
@@ -9,6 +9,11 @@ const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
 
 export default class SignInScreen extends React.Component {
+  state = {
+    id: "",
+    pw: ""
+  };
+
   render() {
     return (
       <SafeAreaView style={GlobalStyles.container}>
@@ -19,27 +24,59 @@ export default class SignInScreen extends React.Component {
             style={SignInStyles.textInput}
             placeholder="아이디"
             placeholderTextColor='#888888'
-            onChangeText={(text)=>this.setState({username:text})}
+            onChangeText={(id)=>this.setState({id: id})}
           />
           <TextInput
             style={SignInStyles.textInput}
             placeholder="비밀번호"
             placeholderTextColor='#888888'
             secureTextEntry={true}
+            onChangeText={(pw) => this.setState({pw: pw})}
           />
           <TouchableOpacity
+            onPress={() => {
+              // 공백 제거
+              const id = this.state.id.trim();
+              const pw = this.state.pw.trim();
+
+              if(id == "") {
+                Alert.alert("아이디를 입력하세요.");
+                return false;
+              } else if(pw == "") {
+                Alert.alert("비밀번호를 입력하세요.");
+                return false;
+              }
+
+              /*로그인 루틴
+              POST to /profile/login (parameter: {id: id, pw: pw}) {
+                if(success) {
+                  this.props.navigation.navigate("Tab");
+                } else {
+                  Alert.alert("아이디 또는 비밀번호가 올바르지 않습니다.");
+                }
+              }*/
+            }}
             activeOpacity={0.8}
             hitSlop={{top:15,bottom: 15, left: 15, right: 15}}
-            style={SignInStyles.button}
-          >
+            style={SignInStyles.button}>
             <Text style={{color:'white'}}>Sign In</Text>
           </TouchableOpacity>
 
           {/* 회원가입창으로 넘어가는 부분 */}
-          <View style={{marginTop:4,width: 400,flexDirection:'row', justifyContent: 'space-around'}}>
-            <Text style={{fontSize:10}}>학점관리요정이 처음이신가요?</Text>
+          <View style={{
+            marginTop:4,
+            width: 400,
+            flexDirection:'row',
+            justifyContent: 'space-around',
+            paddingHorizontal: 20
+          }}>
+            <Text style={{fontSize:14}}>학점관리요정이 처음이신가요?</Text>
             <Text
-              style={{color:'#675CF6',fontSize:10, textDecorationLine:'underline'}}
+              style={{
+                color:'#675CF6',
+                fontSize:14,
+                textDecorationLine: "underline",
+              }}
               onPress={() => {
             // SignUp 으로 갈 수 있는 링크 생성할 것 = 계정 생성
               this.props.navigation.navigate("SignUp");
--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -4,6 +4,13 @@ import GlobalStyles from "../styles/GlobalStyles";
 import SignInStyles from "../styles/SignInStyles";
 
 export default class SignUpScreen extends React.Component {
+  state = {
+    id: "",
+    pw: "",
+    pw_: "",
+    name: ""
+  };
+
   render() {
     return (
       <View style={GlobalStyles.container}>
@@ -13,35 +20,71 @@ export default class SignUpScreen extends React.Component {
           <TextInput
             style={SignInStyles.textInput}
             placeholder='아이디'
+            onChangeText={(id) => this.setState({id: id})}
             // onChangeText={(text)=>this.setState({username:text})}
           />
           <TextInput
             style={SignInStyles.textInput}
+            secureTextEntry={true}
             placeholder='비밀번호'
+            onChangeText={(pw) => this.setState({pw: pw})}
           />
            <TextInput
             style={SignInStyles.textInput}
+            secureTextEntry={true}
             placeholder='비밀번호 확인'
+            onChangeText={(pw_) => this.setState({pw_: pw_})}
            />
            <TextInput
             style={SignInStyles.textInput}
             placeholder='이름'
+            onChangeText={(name) => this.setState({name: name})}
           />
-          <View>
-            <Text
-              style={{fontSize:8, color:'#888888'}}>학점관리요정을 사용하려면 학교인증이 필요합니다.</Text>
-            <TouchableOpacity
-              activeOpacity={0.8}
-              style={SignInStyles.authorization}>
-              <Text style={{color:'white'}}>한양대학교 학생 인증하기</Text>
-            </TouchableOpacity>
-          </View>
+          {/* 한양대학교 인증 확인 버튼 */}
+          {/*<View>*/}
+          {/*  <Text*/}
+          {/*    style={{fontSize:14, color:'#888888'}}>학점관리요정을 사용하려면 학교인증이 필요합니다.</Text>*/}
+          {/*  <TouchableOpacity*/}
+          {/*    activeOpacity={0.8}*/}
+          {/*    style={SignInStyles.authorization}>*/}
+          {/*    <Text style={{color:'white'}}>한양대학교 학생 인증하기</Text>*/}
+          {/*  </TouchableOpacity>*/}
+          {/*</View>*/}
           <TouchableOpacity
             activeOpacity={0.8}
             style={SignInStyles.button}
             onPress={() => {
-              // Tab 으로 갈 수 있는 링크 생성할 것 = 회원가입 완료
-              this.props.navigation.navigate("Tab");
+              const id = this.state.id.trim();
+              const pw = this.state.pw.trim();
+              const pw_ = this.state.pw_.trim();
+              const name = this.state.name.trim();
+
+              if(id == "") {
+                Alert.alert("아이디를 입력하세요.")
+                return false;
+              } else if(pw == "") {
+                Alert.alert("비밀번호를 입력하세요.");
+                return false;
+              } else if(pw_ == "") {
+                Alert.alert("비밀번호 확인란을 입력하세요.");
+                return false;
+              } else if(name == "") {
+                Alert.alert("이름을 입력하세요.");
+                return false;
+              } else if(pw != pw_) {
+                Alert.alert("비밀번호가 서로 일치하지 않습니다.");
+                return false;
+              }
+
+              /*회원가입 루틴
+              POST to /profile/post (parameter: {id: id, pw: pw, name: name}) {
+                if(success) {
+                  login 활성화
+                  this.props.navigation.navigate("Tab");
+                } else {
+                  Alert.alert("오류가 발생했습니다. 다시 시도하시기 바랍니다.");
+                }
+              }*/
             }}>
             <Text style={{color:'white'}}>Sign up</Text>
           </TouchableOpacity>
